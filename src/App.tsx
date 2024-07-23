cc6d825730401d17614df7efb7bd2328d9786331
icicle <unknown@icicleling.com>
2024-07-23 22:34:54 +0800

    美化

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import { ChangeEvent, useEffect, useRef, useState } from "react";
+import styled from "styled-components";
+
+const BAR_TOTAL = 24;
 
 function App() {
-  const [buttonText, setButtonText] = useState<"Start" | "Stop">("Start");
-  const [volume, setVolume] = useState(0);
   const intervalNumberRef = useRef(0);
   const analyserNodeRef = useRef<AnalyserNode | undefined>(undefined);
   const dataArrayRef = useRef<Uint8Array>(new Uint8Array(0));
   const isCallInitRef = useRef(false);
   const screenLockRef = useRef<WakeLockSentinel>();
+
+  const [isStart, setIsStart] = useState(false);
+  const [isFirstStart, setIsFirstStart] = useState(true);
+  const [volume, setVolume] = useState(0);
+  const [showSidePanel, setShowSidePanel] = useState(false);
 
   function keepAwake() {
     if (!("wakeLock" in navigator)) return;
@@ -36,7 +42,6 @@ function App() {
   function initAudio() {
     navigator.mediaDevices
       .getUserMedia({ audio: true, video: false })
-      // .getDisplayMedia({ audio: true })
       .then((stream) => {
         const audioContext = new AudioContext();
         const mediaStreamNode = audioContext.createMediaStreamSource(stream);
@@ -71,17 +76,16 @@ function App() {
   }
 
   function handleClick() {
-    if (buttonText === "Start") {
+    if (!isStart) {
       intervalNumberRef.current = window.setInterval(refreshVolume, 100);
-      setButtonText("Stop");
+      setIsStart(true);
       return;
     }
-    if (buttonText === "Stop") {
-      if (intervalNumberRef === undefined) return;
-      setVolume(0);
-      window.clearInterval(intervalNumberRef.current);
-      setButtonText("Start");
-    }
+
+    if (isFirstStart) setIsFirstStart(false);
+    if (intervalNumberRef === undefined) return;
+    window.clearInterval(intervalNumberRef.current);
+    setIsStart(false);
   }
 
   function handleChangeRange(e: ChangeEvent<HTMLInputElement>) {
@@ -102,6 +106,10 @@ function App() {
     }
   }
 
+  function handleToggleSidePanel() {
+    setShowSidePanel(!showSidePanel);
+  }
+
   useEffect(() => {
     if (isCallInitRef.current) return;
     init();
@@ -109,61 +117,181 @@ function App() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const BAR_TOTAL = 20;
   const activeBarNumber = Math.min(
     BAR_TOTAL,
     Math.floor(volume * (BAR_TOTAL / 100))
   );
-  const barColorArray = Array.from(Array(activeBarNumber).keys()).map(
+  const barColorArr = Array.from(Array(activeBarNumber).keys()).map(
     (num) =>
       `hsl(${280 - 280 * ((activeBarNumber - num) / BAR_TOTAL)}, 80%, 60%)`
   );
 
   return (
-    <div
-      style={{
-        height: "100vh",
-        overflow: "hidden",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        background: "#282830",
-        color: "#fff",
-      }}
-    >
-      <div
-        style={{
-          height: "500px",
-          width: "120px",
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "flex-end",
-        }}
-      >
-        {barColorArray.map((color, index) => (
-          <div
-            key={index}
-            style={{
-              height: `calc(500px / ${BAR_TOTAL} - 4px)`,
-              background: color,
-              marginTop: "4px",
-            }}
-          ></div>
+    <Root>
+      {!isStart && (
+        <Header>
+          <div onClick={fullScreen}>全屏</div>
+          <div onClick={handleToggleSidePanel}>设置</div>
+        </Header>
+      )}
+      <SidePanel $isActive={showSidePanel}>
+        <PanelClose onClick={handleToggleSidePanel}>关闭</PanelClose>
+        <InputRangeWrapper>
+          <InputRange
+            type="range"
+            min={0}
+            max={140}
+            onChange={handleChangeRange}
+          />
+        </InputRangeWrapper>
+        <PanelSettingTitle>灵敏度</PanelSettingTitle>
+      </SidePanel>
+      <MusicBarContainer>
+        {barColorArr.map((color, index) => (
+          <MusicBar key={index} $color={color}></MusicBar>
         ))}
-      </div>
-      <div>
-        {activeBarNumber},{analyserNodeRef.current?.maxDecibels},
-        {analyserNodeRef.current?.minDecibels}
-      </div>
-      <div>{volume}</div>
-      <div style={{ display: "flex", gap: "8px" }}>
-        <button onClick={handleClick}>{buttonText}</button>
-        <button onClick={fullScreen}>全屏</button>
-      </div>
-      <input type="range" max={140} min={0} onChange={handleChangeRange} />
-    </div>
+      </MusicBarContainer>
+      <StartMask onClick={handleClick}>
+        {!isStart && isFirstStart && "点击开始"}
+        {!isStart && !isFirstStart && "暂停"}
+      </StartMask>
+    </Root>
   );
 }
+
+const Root = styled.div`
+  height: 100vh;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background: #282830;
+  position: relative;
+`;
+
+const Header = styled.div`
+  position: absolute;
+  top: 0px;
+  font-size: 22px;
+  color: #fff;
+  z-index: 1;
+  opacity: 0.8;
+  display: flex;
+  width: 100%;
+  padding: 12px;
+  gap: 8px;
+  justify-content: space-between;
+`;
+
+const SidePanel = styled.div<{ $isActive: boolean }>`
+  position: fixed;
+  width: 100vw;
+  height: 100vh;
+  background: #4a4f59;
+  transition: left 0.5s ease;
+  z-index: 1;
+  padding: 24px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  left: ${(props) => (props.$isActive ? 0 : "-100vw")};
+  color: #ffffffaa;
+`;
+
+const PanelClose = styled.div`
+  position: absolute;
+  top: 16px;
+  right: 16px;
+  font-size: 22px;
+`;
+
+const PanelSettingTitle = styled.div`
+  font-size: 32px;
+  margin-top: 1em;
+`;
+
+const InputRangeWrapper = styled.div`
+  position: relative;
+  height: 20rem;
+  width: 3rem;
+
+  &::before,
+  &::after {
+    display: block;
+    position: absolute;
+    color: #fff;
+    width: 100%;
+    text-align: center;
+    font-size: 1.5rem;
+    line-height: 1;
+    padding: 0.75rem 0;
+    pointer-events: none;
+  }
+  &::before {
+    content: "+";
+  }
+  &::after {
+    content: "-";
+    bottom: 0;
+  }
+`;
+
+const InputRange = styled.input`
+  -webkit-appearance: none;
+  appearance: none;
+  background-color: rgba(255, 255, 255, 0.2);
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  margin: 0;
+  padding: 0;
+  width: 20rem;
+  height: 3.5rem;
+  transform: translate(-50%, -50%) rotate(-90deg);
+  border-radius: 1rem;
+  overflow: hidden;
+  cursor: row-resize;
+  &::-webkit-slider-thumb {
+    -webkit-appearance: none;
+    appearance: none;
+    width: 0;
+    box-shadow: -20rem 0 0 20rem rgba(255, 255, 255, 0.2);
+  }
+  &::-moz-range-thumb {
+    border: none;
+    width: 0;
+    box-shadow: -20rem 0 0 20rem rgba(255, 255, 255, 0.2);
+  }
+`;
+
+const MusicBarContainer = styled.div`
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+`;
+
+const MusicBar = styled.div<{ $color: string }>`
+  height: calc(100vh / ${BAR_TOTAL} - 4px);
+  margin-top: 4px;
+  background: ${(props) => props.$color};
+`;
+
+const StartMask = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  font-size: 42px;
+  font-weight: bold;
+  color: #fff;
+  opacity: 0.8;
+  user-select: none;
+`;
 
 export default App;
