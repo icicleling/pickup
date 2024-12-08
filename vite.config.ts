238345bdfada56dcdb022d3ccf4717ae0e807733
icicle <unknown@icicleling.com>
2024-12-09 01:14:49 +0800

    pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import basicSsl from "@vitejs/plugin-basic-ssl";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), basicSsl()],
+  plugins: [
+    react(),
+    basicSsl(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: false,
+    }),
+  ],
   server: {
     host: "0.0.0.0",
   },
